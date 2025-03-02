c462435baf560f41cc16cd9184dbb700fcaaa2a4
Leanstix <leanstixx@gmail.com>
2025-03-02 21:39:39 +0100

    working on the landing page

---
+++ components/LandingPage/landingPage.js
@@ -0,0 +1,14 @@
+"use client";
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { cookies } from 'next/headers';
+import Background from '@/public/background.jpg'
+
+export default function LandingPage() {
+    const router = useRouter();
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+
+    })
+}
