8b2cf2ddc8ee6b8781e69be784bccc1e09777822
Leanstix <leanstixx@gmail.com>
2025-03-05 15:32:57 +0100

    working on the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,6 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      fontFamily: {
-        bungee: ['Bungee', 'cursive'],
-      },
     },
   },
   plugins: [],
