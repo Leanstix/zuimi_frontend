3cb061757a11739b2266fe34aa533a3b4906af6d
Leanstix <leanstixx@gmail.com>
2025-03-05 18:36:55 +0100

    still working on the home page

--- components/LandingPage/components/firstPage.js
+++ components/LandingPage/components/firstPage.js
@@ -5,7 +5,7 @@ export default function HeroSection() {
     return (
         <>
 
-            <div className="relative w-full">
+            <div className="relative w-full h-screen">
                 <div className="container mx-auto h-full relative">
                     {/* Background Image */}
                     <Image
